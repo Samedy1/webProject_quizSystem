36e0e9f2bc93ce263f79f91354be6b1fae6842e5
Samedy1 <samedyphin1@gmail.com>
2022-07-25 13:46:16 +0700

    modified route files

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/subjects', [SubjectController::class, 'index']);
+
+Route::get('/questions/{subject_id}', [QuestionController::class, 'index']);
