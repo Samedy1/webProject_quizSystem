8513cb651063adebecb96c622081b9ceab866764
Samedy1 <samedyphin1@gmail.com>
2022-07-25 13:44:00 +0700

    added index and result functions to QuestionController

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Question;
 
 class QuestionController extends Controller
 {
-    //
+    public function index($subject_id) {
+        $questions = Question::where('subject_id', $subject_id)->get();
+
+        // return $questions;
+
+        return view('questions.index', ['questions' => $questions]);
+    }
+
+    public function result() {
+        $questions = json_decode(request('questions'));
+        $selected_choices = [];
+        $correct_options = [];
+        
+        foreach($questions as $question) {
+            $selected_choices[] = request('selected_choice'.$question->id);
+            $correct_options[] = $question->correct_option;
+        }
+
+        return view('questions.result', [
+            'selected_choices' => $selected_choices,
+            'correct_options' => $correct_options
+        ]);
+    }
 }
