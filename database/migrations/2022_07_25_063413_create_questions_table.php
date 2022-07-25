482b0ff46f7c903203935a2e5ccf7ca30ee75f92
Samedy1 <samedyphin1@gmail.com>
2022-07-25 13:35:10 +0700

    added migrations for subjects and questions table

---
+++ database/migrations/2022_07_25_063413_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('question');
+            $table->json('choices');
+            $table->string('correct_option');
+            $table->unsignedBigInteger('subject_id');
+            $table->foreign('subject_id')->references('id')->on('subjects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
