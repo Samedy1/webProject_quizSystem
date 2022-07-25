bf62b47ff0b1da97fb0196bbf1863de238c77d9b
Samedy1 <samedyphin1@gmail.com>
2022-07-25 19:28:14 +0700

    added nullable constraint to img column of subjects table

--- database/migrations/2022_07_25_063328_create_subjects_table.php
+++ database/migrations/2022_07_25_063328_create_subjects_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->string('img');
+            $table->string('img')->nullable();
             $table->string('description')->nullable();
         });
     }
