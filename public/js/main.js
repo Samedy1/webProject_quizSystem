f3a1795378900e6dfb09a616e7ed261d9f2eb456
sovankanha <sovankanha.nov@student.cadt.edu.kh>
2022-07-27 14:20:53 +0700

    added css img and js inside public folder

--- public/js/main.js
+++ public/js/main.js
@@ -19,3 +19,38 @@ toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+
+
+// Mode
+
+var icon = document.getElementById("icon");
+
+        icon.onclick = function(){
+            document.body.classList.toggle("dark-theme");
+            if(document.body.classList.contains("dark-theme")){
+                icon.src = "sun.png";
+            }else{
+                icon.src = "moon.png";
+                
+            }
+        }
+// Colorful
+
+document.querySelectorAll('.theme-colors .color').forEach(color =>{
+  color.onclick = () => {
+      let background = color.style.background;
+      document.querySelector(':root').style.setProperty('--crimson', background);
+  }
+}); 
+
+let theme = document.querySelector('.themes-container');
+
+document.querySelector('#theme-open').onclick = () =>{
+  theme.classList.add('active');
+  document.body.style.paddingRight = '350px';
+}
+
+document.querySelector('#theme-close').onclick = () =>{
+  theme.classList.remove('active');
+  document.body.style.paddingRight = '0px';
+}
