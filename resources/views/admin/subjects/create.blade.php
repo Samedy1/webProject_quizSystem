4b6b242e85ca35bc1033fdd683281a1602174736
Samedy1 <samedyphin1@gmail.com>
2022-07-31 19:55:30 +0700

    added some style to subject form

--- resources/views/admin/subjects/create.blade.php
+++ resources/views/admin/subjects/create.blade.php
@@ -5,15 +5,15 @@
     <div class="container">
 
         <h1 class="heading">Create a Subject</h1>
-        <form action="{{ route('admin.subjects') }}" method="POST" enctype="multipart/form-data">
+        <form class="create-subject" action="{{ route('admin.subjects') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <label for="subject_name">Subject Name</label>
             <input type="text" name="subject_name" id="subject_name" required>
             <label for="subject_img">Subject Image</label>
             <input type="file" name="subject_img" id="subject_img">
             <label for="subject_desc">Subject Description</label>
-            <input type="text" name="subject_desc" id="subject_desc">
-            <button>Submit</button>
+            <textarea name="subject_desc" id="subject_desc" rows="4" cols="40"></textarea>
+            <button class="btn">Submit</button>
         </form>
     
     </div>
