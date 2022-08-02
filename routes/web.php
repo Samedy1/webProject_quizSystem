105f6ad4e41a696a43b1532e55325a8c5670fec6
Samedy1 <samedyphin1@gmail.com>
2022-08-02 10:40:54 +0700

    laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SubjectController;
-use App\Http\Controllers\QuestionController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,49 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
 Route::get('/dashboard', function () {
-    return view('dashboard.index');
-});
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-
-Route::get('/subjects', [SubjectController::class, 'index'])->name('subjects.index');
-
-
-
-
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard.index');
-})->name('admin.dashboard');
-Route::get('/admin/subjects', [SubjectController::class, 'admin_index'])->name('admin.subjects');
-Route::get('/admin/subjects/create', [SubjectController::class, 'create'])->name('admin.subjects.create');
-Route::post('/admin/subjects', [SubjectController::class, 'store'])->name('admin.subjects.store');
-Route::get('/admin/subjects/show/{subject_id}', [SubjectController::class, 'show'])->name('admin.subjects.show');
-Route::delete('/admin/subjects/{subject_id}', [SubjectController::class, 'destroy'])->name('admin.subjects.destroy');
-Route::get('/admin/subjects/edit/{subject_id}', [SubjectController::class, 'edit'])->name('admin.subjects.edit');
-Route::post('/admin/subjects/update/{subject_id}', [SubjectController::class, 'update'])->name('admin.subjects.update');
-
-
-
-
-Route::get('/questions/{subject_id}', [QuestionController::class, 'index']);
-Route::post('/questions/result', [QuestionController::class, 'result']);
-
-Route::get('/admin/questions')->name('admin.questions');
-Route::get('/admin/questions/create/{subject_id}', [QuestionController::class, 'create'])->name('admin.questions.create');
-Route::post('/admin/questions', [QuestionController::class, 'store'])->name('admin.questions.store');
-Route::delete('/admin/questions/{question_id}', [QuestionController::class, 'destroy'])->name('admin.questions.destroy');
-Route::get('/admin/questions/edit/{question_id}', [QuestionController::class, 'edit'])->name('admin.questions.edit');
-Route::post('/admin/questions/update/{question_id}', [QuestionController::class, 'update'])->name('admin.questions.update');
-
-
-Route::get('/settings', function () {
-    return view('settings.index');
-});
-
-
-Route::get('/history/user', function () {
-    return view('history.show');
-});
-
+require __DIR__.'/auth.php';
