63e1b1f4b0e17819696b1dfe86ef00b099ede7f8
Samedy1 <samedyphin1@gmail.com>
2022-08-08 20:18:28 +0700

    added style to history view

--- resources/views/histories/index.blade.php
+++ resources/views/histories/index.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.layout')
 @section('content')
-    <h1>History of {{ Auth::user()->name }}</h1>
+    <div class="history wrapper">
+        <h1 class="mb-5 h1">History of {{ Auth::user()->name }}</h1>
 
-    <table style="border: 1px solid;">
-        <thead>
-            <tr style="border-bottom: 1px solid;">
-                <td>score</td>
-                <td>subject</td>
-                <td>date</td>
-            </tr>
-        </thead>
-        @foreach ($histories as $history)
-            <tr style="border-bottom: 1px solid;">
-                <td>{{ $history->score }}</td>
-                <td>{{ $history->subject }}</td>
-                <td>{{ $history->created_at}}</td>
-            </tr>
-        @endforeach
-    </table>
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Subject</th>
+                <th scope="col">Score</th>
+                <th scope="col">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($histories as $history)
+                <tr>
+                    <th scope="row">{{ $loop->index + 1 }}</td>
+                    <td>{{ $history->subject }}</td>
+                    <td>{{ $history->score}}</td>
+                    <td>{{ $history->created_at}}</td>
+                    <td><a class="text-primary" href="">See more...</a></td>
+                </tr>
+            @endforeach
+            </tbody>
+          </table>
+          <button class="btn bg-primary text-white">Clear History</button>
+    </div>
 
 @endsection
