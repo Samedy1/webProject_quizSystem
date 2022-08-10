5d426d986dfaa1e3c0af62e4a92e07ae3375065b
sovankanha <sovankanha.nov@student.cadt.edu.kh>
2022-08-10 14:14:37 +0700

    add style question layout

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.layout')
 @section('content')
 <div class="wrapper question-form">
-    {{-- pass subject_id and user_id to store() of HistoryController --}}
-    <form action="{{ route('histories.store', [$info['subject_id'], Auth::user()->id]) }}" method="POST">
+    <form action="/questions/result" method="POST">
         @csrf
         {{-- send questions to the controller for handling the attribute like id, correct option --}}
-        <input type="checkbox" id="questions" name="questions" checked value="{{ $info['questions'] }}" style="display:none;">
-        <h1>Questions</h1>
-        <div>
-            
-            @foreach($info['questions'] as $question)
-                <div class="wrapper bd-r-10 box-shadow">
-                    <div class="title">{{ $question->question}}</div>
+        <input type="checkbox" id="questions" name="questions" checked value="{{ $questions }}" style="display:none;">
+        <h1 class="heading">Questions</h1>
+        <div class="question-user-item">
+            <div>
+            @foreach($questions as $question)
+                <div class="wrapper-cut">
+                    <div class="title-user-question">{{ $question->question}}</div>
                     @foreach($question->choices as $choice)
+                    <div class="question-user-choice">
                         <input type="radio" id="{{ $question->id }}-{{ $choice }}" name="selected_choice{{$question->id}}" value="{{ $choice }}" required>
                         <label for="{{ $question->id }}-{{ $choice }}">{{ $choice }} - selected_choice{{$question->id}}</label><br>
+                    </div>
                     @endforeach
                 </div>
+            
             @endforeach
-        </div>
-        <button>Submit</button>
+            </div>
+        <button class="btn-question-submit">Submit</button>
     </form>
 </div>
 @endsection
